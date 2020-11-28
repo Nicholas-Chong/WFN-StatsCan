031acf3bc2ae2739c3258e52a5d64c9a92e65c74
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2020-11-27 21:09:33 -0500

    First draft of graph component

---
+++ src/graph_page.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Chart from 'chart.js'
+import './graph.css'
+
+class Graph extends React.Component {
+    chartRef = React.createRef();
+    
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+
+        var gradient = myChartRef.createLinearGradient(0, 0, 0, 1400)
+        gradient.addColorStop(0, 'rgba(255, 0,0, 0.5)');
+        gradient.addColorStop(0.5, 'rgba(255, 0, 0, 0.25)');
+        gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
+        
+        new Chart(myChartRef, {
+            backgroundColor: "black",
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["Jan", "Feb", "March"],
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: [86, 67, 91],
+                        backgroundColor: gradient,
+                        pointBackgroundColor: 'white',
+                        borderWidth: 1,
+                        borderColor: '#911215',
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+                scales: {
+                    xAxes: [{
+                        display: false
+                    }],
+                    yAxes: [{
+                        display: false,
+                    }]
+                },
+                legend: {
+                    display: false
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className='Graph-container'>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
+
+export default Graph
